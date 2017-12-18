c9324813b1ce90c10e8b36612c284aa038843b36
Carlos Henrique <carloshlfzanon@gmail.com>
2017-12-18 16:36:18 -0200

    Programinha simples para remoção de acentos de arquivos enviados como argumentos...

---
+++ removeAcentos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace removeAcentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lstArgs = args.ToList();
+            lstArgs.ForEach(arg =>
+            {
+                using (StreamReader srInput = new StreamReader(arg))
+                using (StreamWriter swOutput = new StreamWriter(arg + ".output"))
+                {
+                    List<string> lstInputData = srInput.ReadToEnd().Split('\n').ToList();
+                    srInput.Close();
+
+                    lstInputData.ForEach(lineData =>
+                    {
+                        swOutput.WriteLine(StringParser.removeAcentos(lineData.Trim()));
+                        swOutput.Flush();
+                    });
+
+                    swOutput.Close();
+                }
+            });
+        }
+    }
+}
---
+++ removeAcentos/StringParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace removeAcentos
+{
+    public static class StringParser
+    {
+        /// <summary>
+        /// Remove todos os sinais.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string removeSinais(string text, bool removeAcentos = false)
+        {
+            if (removeAcentos)
+                text = StringParser.removeAcentos(text);
+
+            return Regex.Replace(text, regexSinais, "").Trim();
+        }
+
+        /// <summary>
+        /// Remove acentos de uma string especifica.
+        /// </summary>
+        /// <param name="text">Texto para ser removido os acentos.</param>
+        /// <returns>Texto como os acentos removidos.</returns>
+        public static string removeAcentos(string text)
+        {
+            for (int i = 0; i < regexAcentos.Length; i++)
+            {
+                text = Regex.Replace(text, regexAcentos[i], regexReplace[i]);
+            }
+
+            return text.Trim();
+        }
+
+        private static string regexSinais = "[-=_+.,;:/]";
+
+        /// <summary>
+        /// Acentos para o remove acentos.
+        /// </summary>
+        private static string[] regexAcentos = new List<string> {
+            "[ÂÀÁÄÃ]", "[âãàáä]", "[ÊÈÉË]", "[êèéë]",
+            "[ÎÍÌÏ]", "[îíìï]", "[ÔÕÒÓÖ]", "[ôõòóö]",
+            "[ÛÙÚÜ]", "[ûúùü]", "[Ç]", "[ç]", "[“]",
+            "[”]",
+        }.ToArray();
+
+        /// <summary>
+        /// Valor de troca para os acentos.
+        /// </summary>
+        private static string[] regexReplace = new List<string> {
+            "A", "a", "E", "e",
+            "I", "i", "O", "o",
+            "U", "u", "C", "c",
+            "\"", "\"",
+        }.ToArray();
+    }
+}
